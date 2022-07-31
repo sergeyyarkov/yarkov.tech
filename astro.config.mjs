e1d6f776f241c71a528cc602cb6c8f7393b11688
Sergey Yarkov <serzh.yarkov@gmail.com>
2022-08-01 02:21:11 +0300

    feat: added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yarkov.tech',
-	integrations: [solid()],
+	integrations: [solid(), compress({ img: false })],
 	markdown: {
 		syntaxHighlight: 'prism',
 	},
