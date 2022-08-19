e273f31115c8cd8ef71d7ddbebd99fb06e5bcc99
Sergey Yarkov <serzh.yarkov@gmail.com>
2022-08-19 19:07:58 +0300

    added open graph meta tag for article

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -13,5 +13,6 @@ export function isURL(url: string): boolean {
 export function normalizeLangTag(tag: string) {
 	if (!tag.includes("-")) return tag.toLowerCase();
 	const [lang, region] = tag.split("-");
+
 	return lang.toLowerCase() + "-" + region.toUpperCase();
 }
