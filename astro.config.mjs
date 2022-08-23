605618f90f56a74f39485bc3ae392ab12c8da0bf
Sergey Yarkov <serzh.yarkov@gmail.com>
2022-08-23 16:05:42 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,9 +33,7 @@ export default defineConfig({
 			},
 		}),
 		// image(),
-		compress({
-			html: false,
-		}),
+		compress(),
 		mdx({
 			remarkPlugins: [remarkGfm, remarkSmartypants],
 			rehypePlugins: [
