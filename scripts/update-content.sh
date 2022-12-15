c1fefc30ef3a253c3b5f011d8fd4b53cdb4d8c17
sergeyyarkov <serzh.yarkov@gmail.com>
2022-12-15 21:46:13 +0300

    added some bash scripts

---
+++ scripts/update-content.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd content/
+git commit -m "Update content."
+git push origin main
+cd ../
+git add content/
+git commit -m "Update content"
+git checkout main
+git merge develop
+git checkout develop
+git push origin --all
