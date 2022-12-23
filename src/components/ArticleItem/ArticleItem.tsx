9b96539737be8f337ced06c0c8cc6863c0641661
sergeyyarkov <serzh.yarkov@gmail.com>
2022-12-23 21:07:07 +0300

    fix href in ArticlItem component

--- src/components/ArticleItem/ArticleItem.tsx
+++ src/components/ArticleItem/ArticleItem.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable solid/reactivity */
-import type { Component } from "solid-js";
+import { Component, createEffect } from "solid-js";
 import { createDateFormatter } from "@utils/date";
 import "./ArticleItem.scss";
 import { DEFAULT_LANGUAGE } from "@root/config";
@@ -12,13 +12,13 @@ export type ArticleItemProps = {
 export const ArticleItem: Component<ArticleItemProps> = (props) => {
 	const isNonDefaultLanguage = props.data.lang && props.data.lang !== props.lang;
 	const hrefLangPrefix = props.data.lang && props.data.lang !== DEFAULT_LANGUAGE ? `/${props.data.lang}` : "";
+	const href = `${hrefLangPrefix}/blog/${new Date(props.data.published_at).toISOString().split("T")[0]}/${
+		props.data.slug
+	}/`;
+
 	return (
 		<article itemprop="blogPosts" itemscope itemtype="http://schema.org/BlogPosting" class="article-item">
-			<a
-				href={`${hrefLangPrefix}/blog/${new Date(props.data.published_at).toLocaleDateString("en-CA")}/${
-					props.data.slug
-				}/`}
-			>
+			<a href={href}>
 				<div class="flex">
 					<h3 itemprop="headline">{props.data.title}</h3>
 					{isNonDefaultLanguage && <sup>{props.data.lang.toLocaleUpperCase()}</sup>}
